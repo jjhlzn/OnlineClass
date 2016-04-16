01b5cc935902c7dc12e481a69e4ea6b1ced182a7
jjhlzn <jinjunhang@hotmail.com>
2016-04-16 22:41:11 +0800

    控制台相关

--- Demo/AudioPlayer iOS/AppDelegate.swift
+++ Demo/AudioPlayer iOS/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        application.beginReceivingRemoteControlEvents()
         return true
     }
 
@@ -41,6 +42,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    override func remoteControlReceivedWithEvent(event: UIEvent?) {
+        if let event = event {
+            audioPlayer.remoteControlReceivedWithEvent(event)
+        }
     }
 
 
