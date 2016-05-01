2a1740ab0e97da98b2007a4d598e284686452819
jjhlzn <jinjunhang@hotmail.com>
2016-05-01 22:17:07 +0800

    update

--- ServiceConfiguration.swift
+++ ServiceConfiguration.swift
@@ -72,4 +72,8 @@ class ServiceConfiguration {
     static func GetAlbumImageUrl(urlSuffix: String) -> String {
         return "\(ServiceConfiguration.ImageUrlPrefix)/\(urlSuffix)"
     }
+
+
+
+
 }
