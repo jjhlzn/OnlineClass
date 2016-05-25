d5d0ffca4ea57c7efc1c0ae7084667978cf57ce6
jjhlzn <jinjunhang@hotmail.com>
2016-05-25 10:03:31 +0800

    调整首页

--- Demo/AudioPlayer iOS/FirstPageViewController.swift
+++ Demo/AudioPlayer iOS/FirstPageViewController.swift
@@ -11,12 +11,18 @@ import KDEAudioPlayer
 
 class FirstPageViewController: BaseUIViewController {
     
+    @IBOutlet weak var getMoneyImage: UIButton!
+    @IBOutlet weak var getMoneyLabel: UILabel!
+    @IBOutlet weak var couseLabel: UILabel!
+    @IBOutlet weak var couseImage: UIButton!
 
     @IBOutlet weak var playingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addPlayingButton(playingButton)
+        getMoneyLabel.frame.origin.y = getMoneyImage.frame.origin.y + getMoneyImage.frame.size.height + 7
+        couseLabel.frame.origin.y = couseImage.frame.origin.y + couseImage.frame.size.height + 7
     }
     
     override func audioPlayer(audioPlayer: AudioPlayer, didChangeStateFrom from: AudioPlayerState, toState to: AudioPlayerState) {
