ecf294ee0ad6197393b21a25b633cb97d4256f1a
jjhlzn <jinjunhang@hotmail.com>
2016-06-05 19:56:09 +0800

    敬请期待

--- FunctionCell.swift
+++ FunctionCell.swift
@@ -49,23 +49,23 @@ class FunctionCell: UITableViewCell {
     }
 }
 
-public class ExtendFunctionMananger : NSObject {
+ class ExtendFunctionMananger : NSObject {
     
-    var controller : UIViewController
+    var controller : BaseUIViewController
     
     var functions = [
-                     ExtendFunction(imageName: "commonCard", name: "去刷卡", url: "http://www.baidu.com"),
-                     ExtendFunction(imageName: "up", name: "一键提额", url: "http://www.baidu.com"),
-                     ExtendFunction(imageName: "visa", name: "一键办卡", url: "http://www.weibo.com"),
-                     ExtendFunction(imageName: "cardManage", name: "卡片管理", url: "http://www.weibo.com"),
-                     ExtendFunction(imageName: "creditSearch", name: "信用查询", url: "http://www.weibo.com"),
-                     ExtendFunction(imageName: "mmcSearch", name: "mcc查询", url: "http://www.weibo.com"),
-                     ExtendFunction(imageName: "shopcart", name: "商城", url: "http://www.weibo.com"),
-                     ExtendFunction(imageName: "rmb", name: "缴费", url: "http://www.weibo.com"),
-                     ExtendFunction(imageName: "dollar", name: "贷款", url: "http://www.weibo.com"),
+                     ExtendFunction(imageName: "commonCard", name: "去刷卡", isSupport: true, url: "http://www.baidu.com"),
+                     ExtendFunction(imageName: "up", name: "一键提额", isSupport: true, url: "http://www.baidu.com"),
+                     ExtendFunction(imageName: "visa", name: "一键办卡", isSupport: true, url: "http://www.weibo.com"),
+                     ExtendFunction(imageName: "cardManage", name: "卡片管理", isSupport: false, url: "http://www.weibo.com"),
+                     ExtendFunction(imageName: "creditSearch", name: "信用查询", isSupport: true, url: "http://www.weibo.com"),
+                     ExtendFunction(imageName: "mmcSearch", name: "mcc查询", isSupport: true, url: "http://www.weibo.com"),
+                     ExtendFunction(imageName: "shopcart", name: "商城", isSupport: false, url: "http://www.weibo.com"),
+                     ExtendFunction(imageName: "rmb", name: "缴费", isSupport: false, url: "http://www.weibo.com"),
+                     ExtendFunction(imageName: "dollar", name: "贷款", isSupport: false, url: "http://www.weibo.com"),
                     ]
     
-    public init(controller: UIViewController) {
+    init(controller: BaseUIViewController) {
         self.controller = controller
     }
     
@@ -87,8 +87,17 @@ public class ExtendFunctionMananger : NSObject {
                 let imageView = cell.getImageView(i)
                 imageView.image = UIImage(named: function.imageName)
                 imageView.tag = index
-                imageView.addGestureRecognizer(getTapAction(function))
+                
+                if function.isSupport {
+                    imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imageHandler)))
+                    cell.getLabel(i).addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imageHandler)))
+                } else {
+                    imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(unSupportHandler)))
+                    cell.getLabel(i).addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(unSupportHandler)))
+                }
+                
                 imageView.userInteractionEnabled = true
+                cell.getLabel(i).userInteractionEnabled = true
                 index = index + 1
             }
         }
@@ -98,16 +107,15 @@ public class ExtendFunctionMananger : NSObject {
     
     
     
-    private func getTapAction(function: ExtendFunction) -> UITapGestureRecognizer {
-        let action = UITapGestureRecognizer(target: self, action: #selector(imageHandler))
-        return action
-    }
-    
-    public func imageHandler(sender: UITapGestureRecognizer? = nil) {
+    func imageHandler(sender: UITapGestureRecognizer? = nil) {
         let index = sender?.view?.tag
         let function = functions[index!]
         let params : [String: String] = ["url": function.url, "title": function.name]
         controller.performSegueWithIdentifier("loadWebPageSegue", sender: params)
+    }
+    
+    func unSupportHandler(sender: UITapGestureRecognizer? = nil) {
+        controller.displayMessage("敬请期待")
     }
     
 
@@ -117,11 +125,13 @@ class ExtendFunction {
     var imageName = ""
     var name = ""
     var url = ""
+    var isSupport = false
     var actiono : UITapGestureRecognizer?
     
-    init(imageName: String, name: String, url: String) {
+    init(imageName: String, name: String, isSupport: Bool, url: String) {
         self.imageName = imageName
         self.name = name
+        self.isSupport = isSupport
         self.url = url
     }
 }
